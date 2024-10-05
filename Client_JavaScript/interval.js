ca2ca0ea394e093c39ab842a646f2bddbbc5ca48
RandomHyper <37818950+RandomHyper@users.noreply.github.com>
2024-10-04 21:24:14 -0400

    Update interval.js

--- Client_JavaScript/interval.js
+++ Client_JavaScript/interval.js
@@ -1,21 +1,8 @@
-//      _______                  ____  _              ____        __ 
-//     / _____/ ___  ___ ____   / __ \(_)_______     / __ )____  / /_
-//    / /__   / __/ /__/ ___/  / /_/ / / ___/ _ \   / __  / __ \/ __/
-//   / ___/  / / / /__/ /__/  / _, _/ / /__/  __/  / /_/ / /_/ / /_  
-//  /_/     /_/ /____/____/  /_/ |_/_/\___/\___/  /_____/\____/\__/  
-//  
-//  Original by: Neil Master/Yeehawlerz101
-//  Remixed by: LuisAFK
-//  Make sure to leave your ad blocker off so that Freerice can have some sort of income :)
-//  How to use: Make an account, sign in, then go to the multiplacation table category (freerice.com/categories/multiplication-table)
-//  Then when the page loads, go to the console of your web browser and paste this code in the console and enjoy!
-//  https://github.com/lafkpages/FreericeHack
-
-
 // Define the function
 // Save it as a property of window to prevent re-defining variables
-window.freeRiceHackFunc = function()
-{ 
+let totalrice = document.querySelector("#root > section > div > div.container.page__container.page--game > div > div.game-block > div.globe > div.rice-counter > div.rice-counter__value > span").innerText;
+
+window.freeRiceHackFunc = function() { 
   let problem = document.getElementsByClassName("card-title")[0].innerText; // '11 x 12'
   let pr      = problem.split('x').map(n => parseInt(n)); // [11, 12]
 
@@ -28,31 +15,30 @@ window.freeRiceHackFunc = function()
   let c = opts[2]; // HTMLElement
   let d = opts[3]; // HTMLElement
 
-  if (parseInt(a.innerText) == answer)
-  {
+  if (parseInt(a.innerText) == answer) {
     a.click();
-  }
-  else if (parseInt(b.innerText) == answer)
-  {
+  } else if (parseInt(b.innerText) == answer) {
     b.click();
-  }
-  else if (parseInt(c.innerText) == answer)
-  {
+  } else if (parseInt(c.innerText) == answer) {
     c.click();
-  }
-  else if (parseInt(d.innerText) == answer)
-  {
+  } else if (parseInt(d.innerText) == answer) {
     d.click();
   }
 
-  for (let i = 0; i < window.bruh.length; i++)
-  {
+  // Check if total rice is 20,000
+  totalrice = parseInt(document.querySelector("#root > section > div > div.container.page__container.page--game > div > div.game-block > div.globe > div.rice-counter > div.rice-counter__value > span").innerText);
+  
+  if (totalrice >= 20000) {
+    location.reload(); // Refresh the page
+  }
+
+  for (let i = 0; i < window.bruh.length; i++) {
     clearTimeout(window.bruh[i]);
     bruh.shift();
   }
 
   bruh.push(setTimeout(window.freeRiceHackFunc, 100));
-}
+};
 
 // Define a list to save all the timeouts
 // Save it as a property of window to prevent re-defining variables
